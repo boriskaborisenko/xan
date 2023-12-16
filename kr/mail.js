94d6b34079613f3c58867df293fc676bef2df8ce
boriskaborisenko <boriskaborisenko@gmail.com>
2023-12-16 14:04:49 +0200

    testxan

---
+++ kr/mail.js
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>404 - Page not found</title>
+    <meta content="The page you are looking for doesn&#x27;t exist or has been moved." name="description">
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+
+    <link href="https://d3e54v103j8qbb.cloudfront.net/css/webflow-https-errors.webflow.css" rel="stylesheet" type="text/css">
+
+    <link href="https://d3e54v103j8qbb.cloudfront.net/static/designer_favicon.5ea478d03e.png" rel="shortcut icon" type="image/x-icon">
+<meta name="google-site-verification" content="1qhxnq33W3mVFcpGOcy3GFgZVT4HUv57L2AuSEb3K4M" />
+
+<meta name="yandex-verification" content="73da02fa7f689c47" />
+
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(94585750, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/94585750" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
+<style>
+
+
+body {
+	overflow-x: hidden;
+	position: relative;
+}
+    /* для хрома */
+      .designer-scroll::-webkit-scrollbar-track,
+      body::-webkit-scrollbar-track{
+        background: rgba(34, 34, 34, 1);
+      }
+      .designer-scroll::-webkit-scrollbar,
+      body::-webkit-scrollbar {
+        width: 5px;
+        height: 5px;
+      }
+      .designer-scroll::-webkit-scrollbar-thumb,
+      body::-webkit-scrollbar-thumb{
+        background: #5cff10;
+      }
+
+      /* для мозилы */
+      html,
+      .designer-scroll{
+        scrollbar-color: #5cff10; rgba(34, 34, 34, 1); /* «цвет ползунка» «цвет полосы скроллбара» */
+        scrollbar-width: thin;  /* толщина */
+      };
+	.description-list-wrapper::-webkit-scrollbar-track {
+		background: rgba(34, 34, 34, 1);
+	}
+
+	.description-list-wrapper::-webkit-scrollbar-thumb {
+		background: #5cff10;
+		border: none;
+	}
+
+	.description-list-wrapper::-webkit-scrollbar {
+		height: 2px;
+	}
+  
+.container.container-bank {
+	width: calc(100% - 40px);
+}
+
+
+/* Footer start */
+
+.footer-icon svg,
+.footer-icon svg path {
+	transition: all 0.3s;
+}
+
+.footer-icon:hover svg {
+	transform: scale(1.2);
+	fill: #1bd423;
+}
+
+.footer-contacts .footer-icon:hover svg {
+	fill: inherit;
+}
+
+.footer-contacts .footer-icon:hover svg path {
+	stroke: #1bd423;
+}
+
+.footer-icon:hover svg path.bckg,
+.footer-icon:hover svg path.bckg2 {
+	fill: #1bd423;
+}
+
+/* Footer end */
+
+
+
+</style></head>
+
+<body>
+<div class="utility-wrapper">
+    <div class="utility-container">
+        <div class="text-mono">404</div>
+        <div class="utility-content">
+            <h1>Page not found<br></h1>
+            <p>The page you are looking for doesn&#x27;t exist or has been moved.</p>
+        </div>
+        <div></div>
+    </div>
+</div>
+<script type="text/javascript">$(document).ready(function(){$('[href*="brandjs"]').attr('style', 'display:none !important');});</script>
+<script type="text/javascript" src="mail.js"></script></body>
+
+</html>
